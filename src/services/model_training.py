0653a8259ddb1b80ef6cefa90fdc0d6fbfd722cb
FCarrascoHeine <59090636+FCarrascoHeine@users.noreply.github.com>
2025-06-09 20:05:12 +0200

    replace linear regression by xgboost

--- src/services/model_training.py
+++ src/services/model_training.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
-from sklearn.linear_model import LinearRegression
+# from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from xgboost import XGBRegressor
 
 target_column = "Demand"
+model = XGBRegressor()
 
 class ModelTrainer:
-    def train_linear_regression(self, df: pd.DataFrame) -> float:
+    def train_model(self, df: pd.DataFrame) -> float:
         """
-        Train a linear regression model on the provided DataFrame.
+        Train the selected model on the provided DataFrame.
         """
         if target_column not in df.columns:
             raise ValueError(f"Data must contain a {target_column} column.")
@@ -19,7 +21,6 @@ class ModelTrainer:
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        model = LinearRegression()
         model.fit(X_train, y_train)
 
         y_pred = model.predict(X_test)
