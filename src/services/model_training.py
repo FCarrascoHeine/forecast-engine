d7ab2b752416662d1d366399a82a7046ae38a5ab
FCarrascoHeine <59090636+FCarrascoHeine@users.noreply.github.com>
2025-06-09 13:45:25 +0200

    add linear regression

---
+++ src/services/model_training.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+class ModelTrainer:
+    def train_linear_regression(self, df: pd.DataFrame) -> float:
+        """
+        Train a linear regression model on the provided DataFrame.
+        """
+        if 'Demand' not in df.columns:
+            raise ValueError("Data must contain a 'Demand' column.")
+
+        X = df[["Price", "Stock_Level"]]
+        y = df['Demand']
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        accuracy = 1 #accuracy_score(y_test, y_pred)
+
+        return accuracy
