ca13478e829a8abfb0e9946e4297304e55959422
FCarrascoHeine <59090636+FCarrascoHeine@users.noreply.github.com>
2025-06-09 14:02:13 +0200

    adjust ui

--- src/services/model_training.py
+++ src/services/model_training.py
@@ -9,11 +9,11 @@ class ModelTrainer:
         """
         Train a linear regression model on the provided DataFrame.
         """
-        if 'Demand' not in df.columns:
+        if "Demand" not in df.columns:
             raise ValueError("Data must contain a 'Demand' column.")
 
         X = df[["Price", "Stock_Level"]]
-        y = df['Demand']
+        y = df["Demand"]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
